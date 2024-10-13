5379e2baabc2ac9f05ec86aa4e832c38d0aca51d
Sahil Singh <solankisahil1819@gmail.com>
2024-10-13 15:43:49 +0530

    initial commit

---
+++ NewTestNg/src/Mycode1/NewTestNg1.java
@@ -0,0 +1,19 @@
+package Mycode1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTestNg1 {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
